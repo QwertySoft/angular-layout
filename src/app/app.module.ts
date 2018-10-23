30e808255773d02378e474bc1c565bdd4fee1ce0
Máximo Zarza <zmaximo1990@gmail.com>
2018-10-22 22:46:08 -0300

    Site layouts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { FeaturesModule } from './features/features.module';
-import { PageOneModule } from './features/feature-one/pages/page-one/page-one.module';
-import { PageOneComponent } from './features/feature-one/pages/page-one/page-one.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-
-const appRoutes: Routes = [
-  { path: 'page-one', component: PageOneComponent },
-  { path: '', component: PageOneComponent },
-  { path: '**', component: PageOneComponent }
-];
+import { AppRoutingModule } from './app-routing.module';
+import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    NotFoundPageComponent
   ],
   imports: [
     BrowserModule,
-    FeaturesModule,
-    RouterModule.forRoot(
-      appRoutes,
-      { enableTracing: true } // <-- debugging purposes only
-    )
+    FeaturesModule, // Modulo que segmenta el codigo en modulos de aplicacion
+    AppRoutingModule // Modulo que define las rutas de la app
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FeatureOneModule } from './feature-one/feature-one.module';
+import { LandingModule } from './landing/landing.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    FeatureOneModule.forRoot()
+    FeatureOneModule,
+    LandingModule
   ],
   declarations: []
 })
